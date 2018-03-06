dbd964edc298e3554af6c71fd927fe3aba3aa039
gaojian1 <gaojian1@chelun.com>
2018-03-06 15:07:04 +0700

    fatal error solution

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use App\Models\CoachModel;
 use App\Models\WxCoachExtendModel;
 use App\Util\Request;
 use App\Util\Log;
-use Jeanku\Database\DatabaseManager as DB;
+//use Jeanku\Database\DatabaseManager as DB;
 
 
 /**
@@ -19,18 +19,24 @@ class HomeController extends BaseController
 
     public function home()
     {
+
         try {
-            $data = DB::table('coach')->where('c_id', 10189)->get();
+            test();
+            throw new \Exception('error22', -1);
+//            $data = DB::table('coach')->where('c_id', 10189)->get();
 
 //            $data = DB::select('select * from coach where c_id = 10189', []);
 
 
 //            $data = CoachModel::where('c_id', 10189)->select('c_id')->get()->toArray();
 
-            return $this->success($data);
+//            return $this->success($data);
+//            $this->haha();
+//            return 123;
         } catch (\Exception $e) {
             Log::emergency(__CLASS__. '::' . __FUNCTION__, ['code'=>$e->getCode(), 'massage'=>$e->getMessage()]);
             return $this->error($e->getCode(), $e->getMessage());
         }
     }
+
 }
--- public/index.php
+++ public/index.php
@@ -2,26 +2,37 @@
 // Autoload 自动载入
 require '../vendor/autoload.php';
 
+//error_reporting(E_ALL & ~E_NOTICE);                     //设置错误级别
+
 
 define('WEBPATH', dirname(__DIR__));
 define('ENV', 'dev');
 define('CONFIGPATH',  WEBPATH . '/config/');
 
-\Jeanku\Database\DatabaseManager::make(WEBPATH . '/config/'. ENV . '/database.php');
+//\Jeanku\Database\DatabaseManager::make(WEBPATH . '/config/'. ENV . '/database.php');
 
 // 路由配置
 //require '../config/routes.php';
 
+
+
+//处理fatal error
+register_shutdown_function("error_handler");
+function error_handler(){
+    $error = error_get_last();
+    if($error && ($error["type"]===($error["type"] & (E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR | E_PARSE)))) {
+        responseJson(['code'=>500, 'msg'=>'服务器错误', 'data'=>null]);
+    }
+}
+
+//run mvc
 $request = file_get_contents('php://input');
 $param = json_decode($request, true);
-
-
 if (is_array($param)) {
     responseJson(callFunction($param['controller'], $param['function'], $param['param']));
 } else {
     exit('参数请求参数格式有误');
 }
-
 
 function callFunction($controller, $function, $param)
 {
